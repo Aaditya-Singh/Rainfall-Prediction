b3ec0d2ed56bb6b528eab2fcd3f639138f84db4e
adimeanssun24@gmail.com <singhaadityash@gmail.com>
2021-08-07 18:00:10 +0530

    Found max point in heatmap

--- demo.py
+++ demo.py
@@ -7,26 +7,30 @@ from pytorch_grad_cam import GradCAM
 from pytorch_grad_cam.utils.image import show_cam_on_image
 
 ################################################## LOAD IMAGE ##################################################
-def path_to_image(path):
+def image_from_path(path):
     img = Image.open(path).convert('RGB')
     tensor = transforms.ToTensor()(img)
     tensor = tensor.unsqueeze(0)
     return img, tensor
 
-image_path = '/Users/apple/Downloads/VisualNav/cvmlp-p4/demo_images/both.png'
-image, image_tensor = path_to_image(image_path); # image.show()
-print(image_tensor.shape)
+image_path = '/Users/apple/Downloads/VisualNav/cvmlp-p4/demo_images/dog.jpg'
+image, image_tensor = image_from_path(image_path); # image.show()
+print("Image shape: {}".format(image_tensor.shape))
 image_array = np.asarray(image)/255
 
-################################################ DEFINE MODEL ##################################################
+################################################ OBTAIN HEATMAP #################################################
 model = resnet50(pretrained=True)
 layer = model.layer4[-1]
 
-cam = GradCAM(model=model, target_layer=layer, use_cuda=False)
-category = None
-grayscale_cam = cam(input_tensor=image_tensor, target_category=category)
-grayscale_cam = grayscale_cam[0, :]
+def image_to_heatmap(tensor, model, layer, category=None):
+    cam = GradCAM(model=model, target_layer=layer, use_cuda=False)
+    grayscale_cam = cam(input_tensor=image_tensor, target_category=category)
+    grayscale_cam = grayscale_cam[0, :]
+    max_indices = np.where(grayscale_cam == grayscale_cam.max())
+    max_x = max_indices[0][0]; max_y = max_indices[1][0]
+    return grayscale_cam, [max_x, max_y]
 
+grayscale_cam, [x, y] = image_to_heatmap(image_tensor, model, layer, 230)
 heatmap_array = show_cam_on_image(image_array, grayscale_cam, use_rgb=True)
-heatmap_image = Image.fromarray(heatmap_array)
-heatmap_image.show()
+heatmap_image = Image.fromarray(heatmap_array); heatmap_image.show()
+print("Maximum intensity point: {}, {}".format(x, y))
