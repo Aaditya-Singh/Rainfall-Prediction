e8fb3ac45718a9a2a9eb16a5136f1fcfbada6342
adimeanssun24@gmail.com <singhaadityash@gmail.com>
2021-08-09 12:53:33 +0530

    Parsed xml with multiple objects

--- pascalvoc/readxml.py
+++ pascalvoc/readxml.py
@@ -2,16 +2,26 @@ import xmltodict
 import pandas as pd
 import cv2
 
-path = open('/Users/apple/Downloads/VisualNav/cvmlp-p4/pascalvoc/cat.xml', 'r')
-xml = path.read(); # print(xml)
-dictionary = xmltodict.parse(xml)
-name = dictionary['annotation']['object']['name']; box = {}
-box['xmin'] = int(dictionary['annotation']['object']['bndbox']['xmin'])
-box['ymin'] = int(dictionary['annotation']['object']['bndbox']['ymin'])
-box['xmax'] = int(dictionary['annotation']['object']['bndbox']['xmax'])
-box['ymax'] = int(dictionary['annotation']['object']['bndbox']['ymax'])
-print(name); print(box)
+########################################## BNDBOXES FOR OBJECTS IN XML #########################################
+def annotations_from_xml(xml_path):
+    xml = path.read(); # print(xml)
+    dicts = xmltodict.parse(xml)
+    list_of_boxes = []; list_of_names = []
+    for i in range(len(dicts['annotation']['object'])):
+        object_dict = dicts['annotation']['object'][i]
+        name = object_dict['name']; box = {}
+        box['xmin'] = int(object_dict['bndbox']['xmin'])
+        box['ymin'] = int(object_dict['bndbox']['ymin'])
+        box['xmax'] = int(object_dict['bndbox']['xmax'])
+        box['ymax'] = int(object_dict['bndbox']['ymax'])
+        list_of_boxes.append(box); list_of_names.append(name)
+        print("Class: {}".format(name)); print("Box: {}".format(box))
+        #################################### show image with bndbox ####################################
+        image = cv2.imread('/Users/apple/Downloads/VisualNav/cvmlp-p4/demo_images/human.jpg')
+        cv2.rectangle(image, (box['xmin'], box['ymin']), (box['xmax'], box['ymax']), color=(0, 255, 0), thickness=2)
+        cv2.imshow("bounded", image); cv2.waitKey(0)
+    return list_of_boxes, list_of_names
 
-image = cv2.imread('/Users/apple/Downloads/VisualNav/cvmlp-p4/demo_images/cat.jpg')
-cv2.rectangle(image, (box['xmin'], box['ymin']), (box['xmax'], box['ymax']), color=(0, 255, 0), thickness=2)
-cv2.imshow("bounded", image); cv2.waitKey(0)
+path = open('/Users/apple/Downloads/VisualNav/cvmlp-p4/pascalvoc/human.xml', 'r')
+boxes, names = annotations_from_xml(path)
+print("Clases: {}".format(names)); print("Boxes: {}".format(boxes))
